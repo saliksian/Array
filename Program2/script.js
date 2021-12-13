981e856b63e582659f304cf501176af9362d21f1
saliksian <salikafzal91@gmail.com>
2021-12-13 20:41:21 +0500

    Program2
    Print All Negative Elements of Array

---
+++ Program2/script.js
@@ -0,0 +1,10 @@
+var array = [2,4,-5,67,9,0,-11,5,-12,-13]
+
+console.log("Negative Elements in array");
+for (let index = 0; index < array.length; index++) {
+     if (array[index]<0) {
+          console.log(array[index]);
+
+     }
+     
+}
