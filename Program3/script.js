e91fe63417990c318619c9f7ff021351c31d07ab
saliksian <salikafzal91@gmail.com>
2021-12-14 20:37:35 +0500

    Program3
    Print sum of all array elements

---
+++ Program3/script.js
@@ -0,0 +1,8 @@
+var array = [2,4,-5,67,9,0,-11,5,-12,-13]
+var sum=0;
+
+for (let index = 0; index < array.length; index++) {
+     sum = sum+array[index];
+     
+}
+console.log("Sum of array element: ", sum);
