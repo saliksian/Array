e0776315b31d55c0219208f53364e35d05579931
saliksian <salikafzal91@gmail.com>
2021-12-16 21:23:52 +0500

    Program8
    Copy element from one array to another array

---
+++ Program8/script.js
@@ -0,0 +1,14 @@
+var array = [2, "Ali", -15, 67, "Salik", 100, 115, -110, 12, 13];
+var dupArray=[];
+
+
+
+
+for (let index = 0; index < array.length; index++) {
+  dupArray.push(array[index])
+}
+
+console.log("Original array:", array);
+console.log("Duplicate array:", dupArray);
+
+
