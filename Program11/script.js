7f864cd1af575e1788a5e88a9903dd5bbb46d5cf
saliksian <salikafzal91@gmail.com>
2021-12-17 10:19:47 +0500

    Program11
    Frequency of each element in an array

---
+++ Program11/script.js
@@ -0,0 +1,42 @@
+var dom = document;
+
+var array = ["Ali","Nabeel","Ahmad","Usama","Fatima","Saad","Muneeb","Ali","Saim","Ahmad"];
+
+
+
+var result = dom.getElementById("result")
+
+
+result.innerHTML = array
+var frequency;
+
+
+
+
+function freqElement() {
+
+  for (let index = 0; index < array.length; index++) {
+    frequency=1;
+
+    for (let ind = index+1; ind < array.length; ind++) {
+      if (array[index]==array[ind]) {
+        array.splice(ind,1)
+        frequency++;
+        
+      }
+      
+      
+    }
+    if (frequency>1) {
+      
+    }
+
+    console.log("Frequency of ",array[index], " is:",frequency);
+    
+
+
+  }
+}
+
+
+
